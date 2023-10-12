6f335c58fa901c544ff6d534ee3092be24e0c733
etyangcaleb <calebetyang8@gmail.com>
2023-10-12 03:53:37 +0300

    lessons

---
+++ lesson1/app.js
@@ -0,0 +1,6 @@
+/* basic identation,comments and console logs */
+
+console.log("hello javascript"); //console log outputs hello javascript 
+let a = 10; // assign 10 to a 
+console.log(a);//output 10 
+
---
+++ lesson2/app.js
@@ -0,0 +1,10 @@
+/* user intergration  funtions */
+
+//prompt 
+prompt("exited learning prompt");
+//alert 
+alert("fail not   to strive fo progrss over perfection");
+//confirm 
+confirm("are you  still there");
+
+console.log(Math.floor(Math.random() *100)); // basic math generator input 
